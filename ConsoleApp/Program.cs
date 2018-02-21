245cbecc9414f8317d475b0b0283213ba6da7ab8
ESA <korewaesa@mail.ru>
2018-02-21 18:50:46 +0900

    Create solution

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameCore;
+
+namespace ConsoleApp {
+    class Program {
+        static void Main(string[] args) {
+            TestClass tst = new TestClass();
+            Console.WriteLine(tst.TestNumber);
+            Console.ReadLine();            
+        }
+    }
+}
